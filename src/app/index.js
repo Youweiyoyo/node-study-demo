f1be1f5f644d4568d6b5d85a79207c7ce9ab267e
Youwei <yoyoyo9527@163.com>
2022-06-13 15:14:30 +0800

    组件逻辑拆分

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+const Koa = require('koa')
+const userRouter = require('../router/user.route')
+const app = new Koa()
+
+// 中间件
+app
+  .use((ctx, next) => {
+    ctx.body = 'hello api'
+    next()
+  })
+  // 注册路由
+  .use(userRouter.routes())
+
+module.exports = app
--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
-const Koa = require('koa')
-const app = new Koa()
 const { APP_PORT } = require('./config/config.default')
-const userRouter = require('./router/user.route')
-
-// 中间件
-app
-  .use((ctx, next) => {
-    ctx.body = 'hello api'
-    next()
-  })
-  // 注册路由
-  .use(userRouter.routes())
+const app = require('./app')
 
 app.listen(APP_PORT, () => {
   console.log(`server is running at port http://localhost:${APP_PORT}`)
