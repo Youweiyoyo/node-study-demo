805fb170882a92a6018ec3b1a487bbf510b8477a
Youwei <yoyoyo9527@163.com>
2022-06-13 22:49:01 +0800

    引入 koa-body

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,12 @@
 // 业务层
 const Koa = require('koa')
+const koaBody = require('koa-body')
 const userRouter = require('../router/user.route')
 const app = new Koa()
 
 // 中间件
 app
-  .use((ctx, next) => {
-    ctx.body = 'hello api'
-    next()
-  })
+  .use(koaBody())
   // 注册路由
   .use(userRouter.routes())
 
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,10 @@
 class UserController {
   async register(ctx, next) {
-    ctx.body = '注册成功'
+    console.log(ctx.request.body)
+    // 1. 获取数据
+    // 2. 操作数据库
+    // 3. 返回结果
+    ctx.body = ctx.request.body
   }
   async login(ctx, next) {
     ctx.body = '登录成功'
